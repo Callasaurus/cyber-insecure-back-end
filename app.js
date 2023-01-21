82fc272fdfc39ec4e596f6b89af942f4ec7288a8
Callasaurus <c.sullivandrage@outlook.com>
2023-01-21 17:57:13 +0000

    upload cv functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import router from './routes/applicationRoutes.js'
 import knowledgeRouter from './routes/knowledgeBankRoutes.js'
 import quizRouter from './routes/quizRoutes.js';
+import uploadRouter from './routes/uploads.js';
 import express from "express";
 import morgan from "morgan";
 import cors from 'cors'
@@ -12,10 +13,12 @@ const app = express();
 app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use("/api/jobApplications", router);
 app.use("/api/knowledgeBank", knowledgeRouter);
 app.use("/api/quizzes", quizRouter)
+app.use("/api/files", uploadRouter);
 
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}`);
