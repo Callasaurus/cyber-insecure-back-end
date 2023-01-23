fe42220d4c43c540379c9a6aaddaf4d8b1a7ba10
Callasaurus <c.sullivandrage@outlook.com>
2023-01-23 09:47:04 +0000

    cleaning up files

--- routes/suggestionsRoutes.js
+++ routes/suggestionsRoutes.js
@@ -4,18 +4,7 @@ const suggestionsRouter = express.Router();
 import { postSuggestions } from '../models/suggestions.js';
 
 suggestionsRouter.post('/', async (req, res) => {
-  const postedSuggestion = await postSuggestions(
-    req.body.cv_link,
-    req.body.interview_question_type,
-    req.body.interview_question,
-    req.body.interview_answer,
-    req.body.knowledge_topic,
-    req.body.knowledge_concept,
-    req.body.github_link,
-    req.body.portfolio_link,
-    req.body.quiz_topic,
-    req.body.quiz_question
-  );
+  const postedSuggestion = await postSuggestions(req.body.cv_link, req.body.interview_question_type, req.body.interview_question, req.body.interview_answer, req.body.knowledge_topic, req.body.knowledge_concept, req.body.github_link, req.body.portfolio_link, req.body.quiz_topic, req.body.quiz_question);
   return res.json({ success: true, payload: postedSuggestion });
 });
 
